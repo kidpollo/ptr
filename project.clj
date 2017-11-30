e52e6ed505e0bbf7684bf9aef81b8f12cd10ce97
Francisco Viramontes <kidpollo@gmail.com>
2017-11-29 22:14:27 -0800

    barsdasdgfgfsdfsdfsdsafsadfasdfdsfsdfsdfsdsdfsdfsdfsdfsdafs

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject prt "0.1.4"
+(defproject prt "0.1.5-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :repositories [["releases" {:url "[https://clojars.org/repo](https://clojars.org/repo)"
+  :repositories [["releases" {:url "https://clojars.org/repo"
                               :creds :gpg}]
-                 ["snapshots" {:url "[https://clojars.org/repo](https://clojars.org/repo)"
+                 ["snapshots" {:url "https://clojars.org/repo"
                                :creds :gpg}]])
