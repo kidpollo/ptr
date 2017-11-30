4852ec95b6b5e6d9bb4b580a64acb843c534c30d
Francisco Viramontes <kidpollo@gmail.com>
2017-11-29 22:20:49 -0800

    Version 0.1.7-alpha3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prt "0.1.7-alpha2"
+(defproject prt "0.1.7-alpha3-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
