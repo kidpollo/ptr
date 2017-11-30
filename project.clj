79439024f861579a710669bd3503806d7423fed3
Francisco Viramontes <kidpollo@gmail.com>
2017-11-29 22:37:32 -0800

    Requested PR changes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prt "0.3.0-alpha1-SNAPSHOT"
+(defproject prt "0.3.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
